2cf2295bc1fb453bcac309d440a7391a1a7bedec
Snyder-F8 <fridahnzomo857@gmail.com>
2025-09-05 15:23:58 +0300

    Created a loop code

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// writeCards: returns an array of thank-you messages
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+// countDown: logs numbers from n down to 0
+function countDown(n) {
+  while (n >= 0) {
+    console.log(n);
+    n--;
+  }
+}
+
+// Export functions for tests
+module.exports = {
+  writeCards,
+  countDown
+};
+
